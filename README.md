a94f7de832e7e5ae5a2d72cf67fac0ddcc67693d
Aaron Kincer <kincera@gmail.com>
2023-01-27 16:19:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stimulus-animation-orchestrator
+
+Orchestrates CSS animations using Stimulus to control animations that are immediate, pause rendering to complete and spanned rendered page animations
---
+++ dist/stimulus-animation-orchestrator.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus";
+
+class src_default extends Controller {
+    connect() {
+        console.log("Test connect stimulus-animation-orchestrator");
+    }
+}
+
+export { src_default as default };
