5c11235fc14d2a23b28619a935ab4c9c54acf96f
Aaron Kincer <kincera@gmail.com>
2023-02-23 12:24:32 -0500

    Added test animation to beforeRender and render events

--- imports/callbacks.js
+++ imports/callbacks.js
@@ -35,12 +35,43 @@ export const turboBeforeCacheCallback = function (event) {
 }
 
 export const turboBeforeRenderCallback = async function (event) {
-    console.log('turboBeforeRenderCallback')
-    console.log(event)
-    let thisPageTarget = document.getElementById('test')
-    let nextPageTarget = event.detail.newBody.querySelector("#test")
+    console.log('turboBeforeRenderCallback');
+    console.log(event);
+    let thisPageTarget = document.getElementById('test');
+    let nextPageTarget = event.detail.newBody.querySelector("#test");
     console.log("-> thisPageTarget", thisPageTarget);
+    // Get current coordinates for thisPageTarget
+
+    // Pause rendering
+    event.preventDefault();
+
+    // Move a certain amount (example from MDN White Rabbit)
+    const testKeyframes = new KeyframeEffect(
+        thisPageTarget, // element to animate
+        [
+            { transform: 'translateX(0)' }, // keyframe start
+            { transform: 'translateX(50px)' } // keyframe finish
+        ],
+        { duration: 500, fill: 'forwards' } // keyframe options
+    );
+
+    const testAnimation = new Animation(testKeyframes, document.timeline);
+
+    // Play animation
+    testAnimation.play();
+
+    await testAnimation.finished
+
+    // Get new coordinates of thisPageTarget
+    let box = thisPageTarget.getBoundingClientRect();
+
+    // Set nextPageTarget to match these coordinates
+    nextPageTarget.style.left = box.left
+
     console.log("-> nextPageTarget", nextPageTarget);
+
+    // Resume rendering
+    event.detail.resume();
 }
 
 export const turboBeforeStreamRenderCallback = function (event) {
@@ -50,6 +81,22 @@ export const turboBeforeStreamRenderCallback = function (event) {
 export const turboRenderCallback = function (event) {
     console.log('turboRenderCallback')
     console.log(event)
+
+    // Move the element further
+    let thisPageTarget = document.getElementById('test');
+    const testKeyframes = new KeyframeEffect(
+        thisPageTarget, // element to animate
+        [
+            { transform: 'translateX(0)' }, // keyframe start
+            { transform: 'translateX(50px)' } // keyframe finish
+        ],
+        { duration: 500, fill: 'forwards' } // keyframe options
+    );
+
+    const testAnimation = new Animation(testKeyframes, document.timeline);
+
+    // Play animation
+    testAnimation.play();
 }
 
 export const turboLoadCallback = function (event) {
