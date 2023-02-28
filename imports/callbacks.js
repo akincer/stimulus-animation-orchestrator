6bd223c24c5d94ba7bc28c434384ab5a7a66bc47
Aaron Kincer <kincera@gmail.com>
2023-02-28 16:11:14 -0500

    Code cleanup - deleted unneeded commented out code, added delete operation in render to clear out already executed animations

--- imports/callbacks.js
+++ imports/callbacks.js
@@ -82,51 +82,6 @@ export const turboBeforeRenderCallback = async function (event) {
 
     // Resume rendering
     event.detail.resume();
-
-    /*
-    let thisPageTarget = document.getElementById('test');
-    let boxBefore = thisPageTarget.getBoundingClientRect();
-    let nextBoxBefore = nextPageTarget.getBoundingClientRect();
-    let nextPageTarget = event.detail.newBody.querySelector("#test");
-
-    // Move a certain amount (example from MDN White Rabbit)
-    const testKeyframes = new KeyframeEffect(
-        thisPageTarget, // element to animate
-        [
-            { transform: 'translateX(0)' }, // keyframe start
-            { transform: 'translateX(50px)' } // keyframe finish
-        ],
-        { duration: 500, fill: 'forwards' } // keyframe options
-    );
-
-    const testAnimation = new Animation(testKeyframes, document.timeline);
-
-    // Play animation
-    testAnimation.play();
-
-    await testAnimation.finished
-
-    // Get new coordinates of thisPageTarget
-    let boxAfter = thisPageTarget.getBoundingClientRect();
-
-    // Set nextPageTarget to match these coordinates
-    console.log("-> event.detail.newBody BEFORE setting left", event.detail.newBody);
-    console.log("-> nextPageTarget BEFORE setting left", nextPageTarget);
-    let newLeft = boxAfter.x - 32;
-    nextPageTarget.style.left = newLeft + 'px';
-    storeItem('newLeft', newLeft + 'px');
-    console.log("-> nextPageTarget AFTER setting left", nextPageTarget);
-    console.log("-> event.detail.newBody AFTER setting left", event.detail.newBody);
-    let nextBoxAfter = nextPageTarget.getBoundingClientRect();
-
-    console.log("-> boxBefore", boxBefore);
-    console.log("-> boxAfter", boxAfter);
-    console.log("-> nextBoxBefore", nextBoxBefore);
-    console.log("-> nextBoxAfter", nextBoxAfter);
-
-    // Resume rendering
-    event.detail.resume();
-    */
 }
 
 export const turboBeforeStreamRenderCallback = function (event) {
@@ -141,35 +96,9 @@ export const turboRenderCallback = async function (event) {
         animationController.play();
     }
 
-
-
-    /*
-    let newLeft = fetchItem('newLeft');
-    let newDistance = parseInt(newLeft) + 50;
-
-    // Move the element further
-    let thisPageTarget = document.getElementById('test');
-    //thisPageTarget.left = newLeft;
-    let boxBefore = thisPageTarget.getBoundingClientRect();
-    console.log("-> boxBefore", boxBefore);
-    const testKeyframes = new KeyframeEffect(
-        thisPageTarget, // element to animate
-        [
-            { transform: 'translateX(' + newLeft + ')' }, // keyframe start
-            { transform: 'translateX(' + newDistance + 'px' + ')' } // keyframe finish
-        ],
-        { duration: 500, fill: 'forwards' } // keyframe options
-    );
-
-    const testAnimation = new Animation(testKeyframes, document.timeline);
-
-    // Play animation
-    testAnimation.play();
-    await testAnimation.finished;
-    let afterBox = thisPageTarget.getBoundingClientRect();
-    console.log("-> afterBox", afterBox);
-
-     */
+    for (const subscriber in document.animations['turbo:render']) {
+        delete document.animations['turbo:render'][subscriber];
+    }
 }
 
 export const turboLoadCallback = function (event) {
