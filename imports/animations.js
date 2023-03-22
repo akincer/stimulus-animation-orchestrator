715e7e9c48e85933cacbf0ea1e1a9234de59aa63
Aaron Kincer <kincera@gmail.com>
2023-03-21 21:08:21 -0400

    Troubleshooting color change

--- imports/animations.js
+++ imports/animations.js
@@ -265,6 +265,7 @@ export function getChangeColorFrame(element, position, section, options = {}) {
             frame[property] = startColor;
         else
             frame[property] = midpointColor(startColor, endColor);
+        frame['offset'] = 0;
     }
 
     if (position === positionEnd) {
@@ -273,6 +274,7 @@ export function getChangeColorFrame(element, position, section, options = {}) {
 
         if (section === sectionFirstHalf)
             frame[property] = midpointColor(startColor, endColor);
+        frame['offset'] = 1;
     }
 
     console.log("-> getChangeColorFrame frame", frame);
