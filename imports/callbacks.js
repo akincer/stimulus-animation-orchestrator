f5ef641a7db698157032187da32b3cc0a919cb81
Aaron Kincer <kincera@gmail.com>
2023-03-24 23:51:30 -0400

    Testing replacing postRenderSubscriber with the animated preRenderSubscriber

--- imports/callbacks.js
+++ imports/callbacks.js
@@ -156,7 +156,7 @@ export const turboBeforeRenderCallback = async function (event) {
 
     for (const subscriber in document.animations[turboBeforeRender]) {
         let postRenderSubscriber = event.detail.newBody.querySelector(`#${subscriber}`), keyframeEffectDefinitions = document.animations[turboBeforeRender][subscriber];
-        let preRenderSubscriber = document.getElementById(postRenderSubscriber.id);
+        let preRenderSubscriber = document.getElementById(subscriber);
         console.log("-> turboBeforeRenderCallback postRenderSubscriber", postRenderSubscriber);
         console.log("-> turboBeforeRenderCallback preRenderSubscriber", preRenderSubscriber);
 
