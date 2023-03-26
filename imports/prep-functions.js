c40cb18f5eccc59cd29c326a6640a8837692d465
Aaron Kincer <kincera@gmail.com>
2023-03-26 16:16:33 -0400

    Testing moving default animations into scheduled animations

--- imports/prep-functions.js
+++ imports/prep-functions.js
@@ -1,6 +1,8 @@
 import {schedulePostNextPageRender, schedulePreNextPageRender} from "./constants";
 
 export function doFadeInPrep(element, subscription) {
+    console.log("-> prepwork doFadeInPrep element", element);
+    console.log("-> prepwork doFadeInPrep subscription", subscription);
     if (subscription.schedule === schedulePostNextPageRender)
         element.style.opacity = "0";
 
@@ -9,6 +11,8 @@ export function doFadeInPrep(element, subscription) {
 }
 
 export function doFadeoutPrep(element, subscription) {
+    console.log("-> prepwork doFadeoutPrep element", element);
+    console.log("-> prepwork doFadeoutPrep subscription", subscription);
     if (subscription.schedule === schedulePostNextPageRender)
         element.style.opacity = "1";
 
